6d16509e5dca742311773904d92a4074a51dea8b
JeviRandio <202310097@feudiliman.edu.ph>
2025-03-25 17:22:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,72 @@ document.addEventListener("DOMContentLoaded", function () {
     window.addEventListener("scroll", checkScroll);
     checkScroll(); // Run on page load
 });
+
+
+// Birthdate Field
+function populateYears() {
+    let yearSelect = document.querySelector(".dateField").querySelector(".yearInput");
+    let currentYear = new Date().getFullYear() - 5;
+    for (let i = currentYear; i >= 1950; i--) {
+        let option = document.createElement("option");
+        option.value = i;
+        option.text = i;
+        yearSelect.appendChild(option);
+    }
+}
+
+function populateDays() {
+    let daySelect = document.querySelector(".dateField").querySelector(".dayInput");
+    for (let i = 1; i <= 31; i++) {
+        let option = document.createElement("option");
+        option.value = i;
+        option.text = i;
+        daySelect.appendChild(option);
+    }
+}
+
+populateYears();
+populateDays();
+
+const levelsForm = document.querySelector("#levelsForm");
+const levelsOptions = document.querySelector("#levelsOptions");
+
+function enrollPrimary(){
+	let child = levelsForm.children;
+	child[0].style.display = "block"
+	child[1].style.display = "none"
+	child[2].style.display = "none";
+	levelsOptions.children[0].classList.add("active-level");
+	levelsOptions.children[1].classList.remove("active-level");
+	levelsOptions.children[2].classList.remove("active-level");
+}
+function enrollSecondary(){
+	let child = levelsForm.children;
+	child[0].style.display = "none"
+	child[1].style.display = "block"
+	child[2].style.display = "none";
+	levelsOptions.children[0].classList.remove("active-level");
+	levelsOptions.children[1].classList.add("active-level");
+	levelsOptions.children[2].classList.remove("active-level");
+}
+function enrollTertiary(){
+	let child = levelsForm.children;
+	child[0].style.display = "none"
+	child[1].style.display = "none"
+	child[2].style.display = "block";
+	levelsOptions.children[0].classList.remove("active-level");
+	levelsOptions.children[1].classList.remove("active-level");
+	levelsOptions.children[2].classList.add("active-level");
+}
+
+const params = new URLSearchParams(window.location.search);
+	if (params.get("enroll") === "secondary") {
+		enrollSecondary();
+	}
+	else if (params.get("enroll") === "tertiary") {
+		enrollTertiary();
+	}
+  
+function admissionPage(level){
+	window.location.href = "admission.html?enroll=" + level;
+}
